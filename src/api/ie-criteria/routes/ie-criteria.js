24176dedf44cf430c7c7959d5dcb697344e3c1b7
oleh.trunov <oleh.trunov@gmail.com>
2022-12-08 01:34:07 +0100

    added ie-criteria type

---
+++ src/api/ie-criteria/routes/ie-criteria.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ie-criteria router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ie-criteria.ie-criteria');
---
+++ src/api/ie-criteria/services/ie-criteria.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ie-criteria service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ie-criteria.ie-criteria');
